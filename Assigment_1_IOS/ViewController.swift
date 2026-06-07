6a7601d2bbbcebee47df383fff93e25f26332b00
shani haker <shanihaker98@gmail.com>
2026-06-07 14:15:58 +0300

    Initial Commit

---
+++ Assigment_1_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigment_1_IOS
+//
+//  Created by Shani Haker on 07/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
